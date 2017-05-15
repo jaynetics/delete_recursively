53ffd77f55c06ba258a95739c30caf6b7d2b821e
Jannosch Müller <janosch.mueller@betterplace.org>
2017-05-15 22:24:03 +0200

    force db:schema:load on travis

--- spec/dummy_rails_app/db/schema.rb
+++ spec/dummy_rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110173324) do
+ActiveRecord::Schema.define(version: 20151110173325) do
 
   create_table "blogs", force: :cascade do |t|
   end
