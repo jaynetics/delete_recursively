884bdf09aac9475cc970a16ae46aa8ecc0688941
Akinori MUSHA <knu@idaemons.org>
2024-01-12 12:37:52 +0900

    Test with Rails 7.1 and Ruby up to 3.3
    - Add ActiveRecord 7.1 and Ruby 3.1, 3.2, and 3.3 to the matrix
    - Fix `gem update --system` for Ruby 2.x
    - Simplify ActiveRecord versions

--- spec/dummy_rails_app/config/application.rb
+++ spec/dummy_rails_app/config/application.rb
@@ -9,7 +9,9 @@ module DummyRailsApp
   class Application < Rails::Application
     # silence warning on rails 7
     if (Rails::VERSION::MAJOR >= 7 rescue false)
-      config.active_record.legacy_connection_handling = false
+      if Rails::VERSION::MINOR == 0
+        config.active_record.legacy_connection_handling = false
+      end
     end
   end
 end
